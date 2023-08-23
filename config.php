ad3a03d79025c51fffcba01cbe2d17ec58baaa8b
abuansarpatowary <abuansar040@gmail.com>
2023-08-23 22:35:43 +0600

    Database Config added

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    define("DB_HOST", "localhost");
+    define("DB_USER","root"); // "root" is the default username for the localhost
+    define("DB_PASS", "");
+    define("DB_NAME", "tasks");
+    define("DB_TABLE", "tasks");
+
