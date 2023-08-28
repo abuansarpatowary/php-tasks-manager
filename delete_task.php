c8cf0665d257e30cca1ee5db82260f96575b924c
abuansarpatowary <abuansar040@gmail.com>
2023-08-28 17:54:09 +0600

    remove unnessesary query and code

--- delete_task.php
+++ delete_task.php
@@ -1,1 +1,15 @@
 <?php
+include_once 'config.php';
+$connection = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+// Delete task
+if (isset($_GET['id'])) {
+	$id = $_GET['id'];
+
+	$deleteQuery = "DELETE FROM " . DB_TABLE . " WHERE id = $id";
+	mysqli_query($connection, $deleteQuery);
+
+	header("Location: index.php");
+	exit();
+}
+?>
