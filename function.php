24736e29417a11d433209261a73e9e2953f33879
abuansarpatowary <abuansar040@gmail.com>
2023-08-30 12:14:10 +0600

    status message added for showing login/registration status

---
+++ function.php
@@ -0,0 +1,14 @@
+<?php 
+
+function getStatusMessage($statusCode=0){
+    $status = [
+        '0' => '',
+        '1' => 'User not found',
+        '2' => 'Please fill all the fields',
+        '3' => 'Username Passwords didn\'t match',
+        '4' => 'User already exists',
+        '5' => 'User created successfully',
+    ];
+    return $status[$statusCode];
+
+}
